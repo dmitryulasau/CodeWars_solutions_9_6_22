fe09aa3cf2bb81008d45cd954878749b434a794a
DMITRY ULASAU <dmitrushok@gmail.com>
2022-09-22 19:32:36 -0400

    daily

--- daily.js
+++ daily.js
@@ -227,3 +227,23 @@ console.log(find("leetcode", "leeto"));
 console.log(find("hello", "ll"));
 console.log(find("", ""));
 console.log(find("a", "a"));
+
+// Given a string s, reverse the order of characters in each word within
+// a sentence while still preserving whitespace and initial word order.
+
+// Example 1:
+// Input: s = "Let's take LeetCode contest"
+// Output: "s'teL ekat edoCteeL tsetnoc"
+
+// Example 2:
+// Input: s = "God Ding"
+// Output: "doG gniD"
+
+function reverse(s) {
+  return s
+    .split(" ")
+    .map((element) => element.split("").reverse().join(""))
+    .join(" ");
+}
+
+console.log(reverse("Let's take LeetCode contest"));
